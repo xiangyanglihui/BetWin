4ca684e9f59c8abf5bf6de187bc72047c0a37382
李辉 <xiangyanglihui@gmail.com>
2018-04-03 22:58:39 +0800

    add  test files

---
+++ src/test/java/com/betwin/contract/AccountInfoTest.java
@@ -0,0 +1,14 @@
+package com.betwin.contract;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AccountInfoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/betwin/controller/HelloControllerTest.java
@@ -0,0 +1,14 @@
+package com.betwin.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
