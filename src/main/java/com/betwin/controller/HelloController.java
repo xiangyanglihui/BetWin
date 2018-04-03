093bf4d5cf8999c13c2a0241c0af7b0de90c2f9a
李辉 <xiangyanglihui@gmail.com>
2018-04-03 22:48:38 +0800

    add property file

--- src/main/java/com/betwin/controller/HelloController.java
+++ src/main/java/com/betwin/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.betwin.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/hi")
 public class HelloController {
 
+	@Value("${chain.ip}")
+	private String chainIp;
+	
     @RequestMapping(method = RequestMethod.GET)
     public String sayHello() {
-        return "Hello..........";
+        return "Hello.........." + chainIp;
     }
     
 }
