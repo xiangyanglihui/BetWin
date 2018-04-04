d12ff5d233a1bdc6335fa70cd556e03621b5161d
gerrit2 <hui.h.li@tieto.com>
2018-04-04 13:33:46 +0800

    add Web3j dependency

--- src/main/java/com/betwin/controller/HelloController.java
+++ src/main/java/com/betwin/controller/HelloController.java
@@ -1,20 +1,24 @@
 package com.betwin.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.betwin.service.Web3jService;
 
 @RestController
 @RequestMapping(path = "/hi")
 public class HelloController {
 
-	@Value("${chain.ip}")
-	private String chainIp;
-	
+    @Autowired
+    private Web3jService web3jService;
+    
     @RequestMapping(method = RequestMethod.GET)
-    public String sayHello() {
-        return "Hello.........." + chainIp;
+    public String sayHello() throws IOException {
+        return web3jService.getClientVersion();
     }
     
 }
---
+++ src/main/java/com/betwin/service/Web3jService.java
@@ -0,0 +1,20 @@
+package com.betwin.service;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+
+@Service
+public class Web3jService {
+    
+    @Autowired
+    private Web3j web3j;
+
+    public String getClientVersion() throws IOException{
+        Web3ClientVersion web3ClientVersion = web3j.web3ClientVersion().send();
+        return web3ClientVersion.getWeb3ClientVersion();
+    }
+}
