27650367da895b8126516aff40de1b171fc89996
gerrit2 <hui.h.li@tieto.com>
2018-04-06 05:31:39 +0800

    commit

--- src/main/java/com/betwin/contract/service/Web3jService.java
+++ src/main/java/com/betwin/contract/service/Web3jService.java
@@ -2,19 +2,28 @@ package com.betwin.contract.service;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+import org.web3j.protocol.parity.Parity;
 
 @Service
 public class Web3jService {
+    private static Logger log = LoggerFactory.getLogger(Web3jService.class);
+    
+    @Value("${web3j.client-address:}")
+    private String clientUrl;
     
     @Autowired
-    private Web3j web3j;
+    private Parity web3j;
 
     public String getClientVersion() throws IOException{
-        Web3ClientVersion web3ClientVersion = web3j.web3ClientVersion().send();
-        return web3ClientVersion.getWeb3ClientVersion();
+        Web3ClientVersion client = web3j.web3ClientVersion().send();
+        log.info("**********"+client.getWeb3ClientVersion());
+        log.info("**************" + clientUrl);
+        return "";
     }
 }
